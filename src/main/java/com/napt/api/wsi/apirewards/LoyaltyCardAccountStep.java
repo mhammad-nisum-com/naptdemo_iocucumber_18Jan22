3f6c1404261472714ec0d257141ac67782e75468
Shahid Hussain <SHussain3@wsgc.com>
2021-03-02 19:58:22 +0500

    [WSICCR] Updated step files

--- src/main/java/com/napt/api/wsi/apirewards/LoyaltyCardAccountStep.java
+++ src/main/java/com/napt/api/wsi/apirewards/LoyaltyCardAccountStep.java
@@ -4,29 +4,49 @@ import com.napt.api.wsi.apistores.ApiEngine;
 import com.napt.api.wsi.apistores.Globals;
 import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
-import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-public class Rewards_API_Steps extends ApiEngine {
-    private static final Logger log = Logger.getLogger(Rewards_API_Steps.class);
+/**
+ * The type Loyalty card account step.
+ */
+public class LoyaltyCardAccountStep extends ApiEngine {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoyaltyCardAccountStep.class);
 
+    /**
+     * Verify that the post response code value is for the response with dictionary key and get and.
+     *
+     * @param statusCode      the status code
+     * @param dictionaryKey   the dictionary key
+     * @param responseCode    the response code
+     * @param responseMessage the response message
+     * @throws IOException the io exception
+     */
     @Then("I verify that the post response code value is {string} for the response with Dictionary Key {string} and get {string} and {string}")
     public void iVerifyThatThePostResponseCodeValueIsForTheResponseWithDictionaryKeyAndGetAnd(String statusCode, String dictionaryKey, String responseCode,
                                                                                               String responseMessage) throws IOException {
-
+        LOGGER.debug("Start processing with statusCode:{},dictionaryKey:{},responseCode:{},responseMessage:{}", statusCode, dictionaryKey, responseCode,
+                     responseMessage);
         Response response = (Response) Globals.globalVariables.get(dictionaryKey);
         JSONObject jsonObject = new JSONObject(response.getBody().asString());
         jsonObject = jsonObject.getJSONObject("loyaltyCardCreateResponse").getJSONObject("loyaltyCardAccount");
+        LOGGER.debug("Get jsonObject:{} from response", jsonObject);
         Globals.globalVariables.put("LOYALTY_CARD_ID", jsonObject.getString("loyaltyCardId"));
         JSONObject accountState = jsonObject.getJSONObject("accountState");
+        LOGGER.debug("Get accountState:{} from response", accountState);
         JSONObject cardDetails = jsonObject.getJSONObject("cardDetails");
+        LOGGER.debug("Get cardDetails:{} from response", cardDetails);
         JSONObject earningSummary = jsonObject.getJSONObject("earningSummary");
+        LOGGER.debug("Get earningSummary:{} from response", earningSummary);
         JSONArray cardHolders = cardDetails.getJSONArray("cardHolders");
+        LOGGER.debug("Get cardHolders:{} from response", cardHolders);
         JSONObject memberDetails = cardHolders.getJSONObject(0).getJSONObject("memberDetails");
+        LOGGER.debug("Get memberDetails:{} from response", memberDetails);
         String phone = memberDetails.getString("phone");
         String email = memberDetails.getString("email");
         String firstName = memberDetails.getJSONObject("name").getString("firstName");
@@ -52,16 +72,16 @@ public class Rewards_API_Steps extends ApiEngine {
         Assert.assertTrue(cardDetails.getJSONArray("partnerIds").getJSONObject(0).getBoolean("active"));
         Assert.assertEquals("PRIMARY", cardHolders.getJSONObject(0).getString("cardHoldertype"));
         Assert.assertNotNull(memberDetails.getString("birthday"));
-        Assert.assertNotNull(memberDetails.getString("email"));
-        Assert.assertNotNull(memberDetails.getString("phone"));
+        Assert.assertNotNull(email);
+        Assert.assertNotNull(phone);
         Assert.assertNotNull(memberDetails.getJSONObject("address").getString("country"));
         Assert.assertNotNull(memberDetails.getJSONObject("address").getString("address2"));
         Assert.assertNotNull(memberDetails.getJSONObject("address").getString("city"));
         Assert.assertNotNull(memberDetails.getJSONObject("address").getString("address1"));
         Assert.assertNotNull(memberDetails.getJSONObject("address").getString("postalCode"));
         Assert.assertNotNull(memberDetails.getJSONObject("address").getString("stateProvince"));
-        Assert.assertNotNull(memberDetails.getJSONObject("name").getString("firstName"));
-        Assert.assertNotNull(memberDetails.getJSONObject("name").getString("lastName"));
+        Assert.assertNotNull(firstName);
+        Assert.assertNotNull(lastName);
         Assert.assertNotNull(cardDetails.getString("cardType"));
         Assert.assertNotNull(cardDetails.getString("cardDescription"));
         Assert.assertNotNull(cardDetails.getString("cardProgramCode"));
@@ -69,16 +89,28 @@ public class Rewards_API_Steps extends ApiEngine {
 
     }
 
+    /**
+     * Verify that the get response code value is for the response with dictionary key.
+     *
+     * @param statusCode    the status code
+     * @param dictionaryKey the dictionary key
+     */
     @Then("I verify that the get response code value is {string} for the response with Dictionary Key {string}")
     public void iVerifyThatTheGetResponseCodeValueIsForTheResponseWithDictionaryKey(String statusCode, String dictionaryKey) {
         Response response = (Response) Globals.globalVariables.get(dictionaryKey);
         JSONObject jsonObject = new JSONObject(response.getBody().asString());
         jsonObject = jsonObject.getJSONObject("loyaltyCardGetResponse").getJSONObject("loyaltyCardAccount");
+        LOGGER.debug("Get jsonObject:{} from response", jsonObject);
         JSONObject accountState = jsonObject.getJSONObject("accountState");
+        LOGGER.debug("Get accountState:{} from response", accountState);
         JSONObject cardDetails = jsonObject.getJSONObject("cardDetails");
+        LOGGER.debug("Get cardDetails:{} from response", cardDetails);
         JSONObject earningSummary = jsonObject.getJSONObject("earningSummary");
+        LOGGER.debug("Get earningSummary:{} from response", earningSummary);
         JSONArray cardHolders = cardDetails.getJSONArray("cardHolders");
+        LOGGER.debug("Get cardHolders:{} from response", cardHolders);
         JSONObject memberDetails = cardHolders.getJSONObject(0).getJSONObject("memberDetails");
+        LOGGER.debug("Get memberDetails:{} from response", memberDetails);
 
         Object partnerAccountId = Globals.globalVariables.get("partnerAccountId");
         Assert.assertNotNull(accountState.getString("statusCode"));
@@ -114,50 +146,59 @@ public class Rewards_API_Steps extends ApiEngine {
 
     }
 
+    /**
+     * Verify that the lookup response code value is for the response with dictionary key.
+     *
+     * @param statusCode    the status code
+     * @param dictionaryKey the dictionary key
+     */
     @Then("I verify that the lookup response code value is {string} for the response with Dictionary Key {string}")
     public void iVerifyThatTheLookupResponseCodeValueIsForTheResponseWithDictionaryKey(String statusCode, String dictionaryKey) {
         Response response = (Response) Globals.globalVariables.get(dictionaryKey);
         JSONObject jsonResponse = new JSONObject(response.getBody().asString());
         JSONArray jsonArray = jsonResponse.getJSONObject("loyaltyCardLookupResponse").getJSONArray("loyaltyCardAccountCollection");
-          for (Object o : jsonArray){
-              if(o instanceof JSONObject){
-                  JSONObject jsonObject = (JSONObject) o;
-                  JSONObject accountState = jsonObject.getJSONObject("accountState");
-                  JSONObject cardDetails = jsonObject.getJSONObject("cardDetails");
-                  JSONArray cardHolders = cardDetails.getJSONArray("cardHolders");
-                  JSONObject memberDetails = cardHolders.getJSONObject(0).getJSONObject("memberDetails");
+        for (Object o : jsonArray) {
+            if (o instanceof JSONObject) {
+                JSONObject jsonObject = (JSONObject) o;
+                LOGGER.debug("Get jsonObject:{} from response", jsonObject);
+                JSONObject accountState = jsonObject.getJSONObject("accountState");
+                LOGGER.debug("Get accountState:{} from response", accountState);
+                JSONObject cardDetails = jsonObject.getJSONObject("cardDetails");
+                LOGGER.debug("Get cardDetails:{} from response", cardDetails);
+                JSONArray cardHolders = cardDetails.getJSONArray("cardHolders");
+                LOGGER.debug("Get cardHolders:{} from response", cardHolders);
+                JSONObject memberDetails = cardHolders.getJSONObject(0).getJSONObject("memberDetails");
+                LOGGER.debug("Get memberDetails:{} from response", memberDetails);
 
-                  Object partnerAccountId = Globals.globalVariables.get("partnerAccountId");
-                  Assert.assertNotNull(accountState.getString("statusCode"));
-                  Assert.assertNotNull(accountState.getString("statusInfo"));
-                  Assert.assertNotNull(accountState.getString("lastActivityTime"));
-                  Assert.assertNotNull(accountState.getString("openTime"));
-                  Assert.assertNotNull(accountState.getBoolean("canLogin"));
-                  Assert.assertNotNull(accountState.getBoolean("canEarn"));
-                  Assert.assertNotNull(accountState.getBoolean("canIssueCertificates"));
-                  Assert.assertNotNull(accountState.getBoolean("canRedeem"));
-                  Assert.assertNotNull(accountState.getBoolean("canLogin"));
-                  Assert.assertEquals("COF", cardDetails.getJSONArray("partnerIds").getJSONObject(0).getString("partnerName"));
-                  Assert.assertNotNull(cardDetails.getJSONArray("partnerIds").getJSONObject(0).getString("accountId"));
-                  Assert.assertTrue(cardDetails.getJSONArray("partnerIds").getJSONObject(0).getBoolean("active"));
-                  Assert.assertEquals("PRIMARY", cardHolders.getJSONObject(0).getString("cardHoldertype"));
-                  Assert.assertNotNull(memberDetails.getString("birthday"));
-                  Assert.assertNotNull(memberDetails.getString("email"));
-                  Assert.assertNotNull(memberDetails.getString("phone"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("address").getString("country"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("address").getString("address2"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("address").getString("city"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("address").getString("address1"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("address").getString("postalCode"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("address").getString("stateProvince"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("name").getString("firstName"));
-                  Assert.assertNotNull(memberDetails.getJSONObject("name").getString("lastName"));
-                  Assert.assertNotNull(cardDetails.getString("cardType"));
-                  Assert.assertNotNull(cardDetails.getString("cardDescription"));
-                  Assert.assertNotNull(cardDetails.getString("cardProgramCode"));
-              }
-
-
+                Object partnerAccountId = Globals.globalVariables.get("partnerAccountId");
+                Assert.assertNotNull(accountState.getString("statusCode"));
+                Assert.assertNotNull(accountState.getString("statusInfo"));
+                Assert.assertNotNull(accountState.getString("lastActivityTime"));
+                Assert.assertNotNull(accountState.getString("openTime"));
+                Assert.assertNotNull(accountState.getBoolean("canLogin"));
+                Assert.assertNotNull(accountState.getBoolean("canEarn"));
+                Assert.assertNotNull(accountState.getBoolean("canIssueCertificates"));
+                Assert.assertNotNull(accountState.getBoolean("canRedeem"));
+                Assert.assertNotNull(accountState.getBoolean("canLogin"));
+                Assert.assertEquals("COF", cardDetails.getJSONArray("partnerIds").getJSONObject(0).getString("partnerName"));
+                Assert.assertNotNull(cardDetails.getJSONArray("partnerIds").getJSONObject(0).getString("accountId"));
+                Assert.assertTrue(cardDetails.getJSONArray("partnerIds").getJSONObject(0).getBoolean("active"));
+                Assert.assertEquals("PRIMARY", cardHolders.getJSONObject(0).getString("cardHoldertype"));
+                Assert.assertNotNull(memberDetails.getString("birthday"));
+                Assert.assertNotNull(memberDetails.getString("email"));
+                Assert.assertNotNull(memberDetails.getString("phone"));
+                Assert.assertNotNull(memberDetails.getJSONObject("address").getString("country"));
+                Assert.assertNotNull(memberDetails.getJSONObject("address").getString("address2"));
+                Assert.assertNotNull(memberDetails.getJSONObject("address").getString("city"));
+                Assert.assertNotNull(memberDetails.getJSONObject("address").getString("address1"));
+                Assert.assertNotNull(memberDetails.getJSONObject("address").getString("postalCode"));
+                Assert.assertNotNull(memberDetails.getJSONObject("address").getString("stateProvince"));
+                Assert.assertNotNull(memberDetails.getJSONObject("name").getString("firstName"));
+                Assert.assertNotNull(memberDetails.getJSONObject("name").getString("lastName"));
+                Assert.assertNotNull(cardDetails.getString("cardType"));
+                Assert.assertNotNull(cardDetails.getString("cardDescription"));
+                Assert.assertNotNull(cardDetails.getString("cardProgramCode"));
+            }
         }
     }
 }
--- src/main/java/com/napt/api/wsi/apirewards/TestSteps.java
+++ src/main/java/com/napt/api/wsi/apirewards/TestSteps.java
@@ -2,83 +2,166 @@ package com.napt.api.wsi.apirewards;
 
 import com.napt.api.wsi.apistores.ApiEngine;
 import com.napt.api.wsi.apistores.Globals;
-import com.napt.api.wsi.steps.BrowserSteps;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Then;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.apache.log4j.Logger;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.junit.Assert;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
-public class TestSteps {
-    ApiEngine ae = new ApiEngine();
-    private static final Logger LOGGER = Logger.getLogger(TestSteps.class);
-    private static final Logger log = Logger.getLogger(BrowserSteps.class);
+import static io.restassured.RestAssured.given;
 
+/**
+ * The type Test steps.
+ */
+public class TestSteps extends ApiEngine {
 
-    @When("I make a {string} REST Call with URI {string} and Parameters {string} and {string} and store the response of sessionID with Dictionary Key {string}")
-    public void iMakeARESTCallWithURIAndParametersAndAndStoreTheResponseOfSessionIDWithDictionaryKey(String requestType, String uri, String partnerId, String partnerName, String dictionaryKey) throws URISyntaxException {
-        Response response = (Response) Globals.globalVariables.get("PostUsers");
-        JSONObject postResponse=new JSONObject(response.getBody().asString());
+    /**
+     * That param is set to value with bearer token.
+     *
+     * @param dictionaryKey the dictionary key
+     * @param value         the value
+     */
+    @Given("^that param \"([^\"]*)\" is set to value with bearer token  \"([^\"]*)\"$")
+    public void thatParamIsSetToValueWithBearerToken(String dictionaryKey, String value) {
 
-        String getUri = getUriForGetWithPartner(uri, postResponse, partnerName, partnerId);
-        Response rs = ae.callAPI("get", Globals.headers, "", Globals.globalVariables.get("url").toString() + getUri);
-        Globals.globalVariables.put(dictionaryKey,rs);
+        String[] paramCategory = dictionaryKey.split("\\.");
+        switch (paramCategory[0]) {
+        case "headers":
+            Globals.headers.put(paramCategory[1], value);
+            if (value.contains("access_token")) {
+                Globals.headers.put(paramCategory[1], "Bearer " + Globals.access_token.get("access_token"));
+            }
+            break;
+        case "base_url":
+            Globals.globalVariables.put(paramCategory[1], value);
+            break;
+        case "xml_headers":
+            Globals.xmlHeaders.put(paramCategory[1], value);
+            break;
+        case "Authorization":
+            Globals.Authorization.put(paramCategory[1], value);
+            break;
+        case "sessionheaders":
+            if (value.equals("sessionID")) {
+                Globals.sessionHeaders.put(paramCategory[1], Globals.sessionID.get("SessionID"));
+            } else {
+                Globals.sessionHeaders.put(paramCategory[1], value);
+            }
+            break;
+        case "oauthEncoded":
+            Globals.oAuthEncoded.put(paramCategory[1], value);
+            break;
+
+        }
 
     }
 
-    private String getUriForGetWithPartner(String uri, JSONObject postResponse, String partnerName, String partnerId) {
-        String pName = null;
-        String pId = null;
-        if ("#GetFromResponse".equals(partnerName)) {
-            JSONArray jsonArray = postResponse.getJSONObject("loyaltyCardCreateResponse").getJSONObject("loyaltyCardAccount").getJSONObject("cardDetails").getJSONArray("partnerIds");
-
-        } else {
-            pName = partnerName;
-        }
-        if ("#GetFromResponse".equals(partnerId)) {
-
-        } else {
-            pId = partnerId;
-        }
-
-        return uri+pName+"/"+pId;
+    /**
+     * Read the json from file and replace partner id with random generated into dictionary key.
+     *
+     * @param jsonFilePath  the json file path
+     * @param dictionaryKey the dictionary key
+     * @throws IOException the io exception
+     */
+    @And("I read the JSON from file {string} and replace partner id with random generated into Dictionary Key {string}")
+    public void iReadTheJSONFromFileAndReplacePartnerIdWithRandomGeneratedIntoDictionaryKey(String jsonFilePath, String dictionaryKey) throws IOException {
+        String cwd = System.getProperty("user.dir");
+        String jsonPath = cwd + jsonFilePath;
+        String partnerAccountId = randomGeneratedPartnerId();
+        Globals.globalVariables.put(dictionaryKey, jsonFileToString(jsonPath).replace("{partnerAccountId}", partnerAccountId));
+        Globals.globalVariables.put("partnerAccountId", partnerAccountId);
     }
 
-
-
-    @Then("I verify that the response code value is {string} for the response with Dictionary Key {string} and get {string} and {string}")
-    public void iVerifyThatTheResponseCodeIsForTheResponseWithDictionaryKeyAndGetAnd(String responseCode, String dictionaryKey, String responseObject,String responseMessage) {
-        Response rs = (Response) Globals.globalVariables.get(dictionaryKey);
-        String Actual_message=null;
-        System.out.println("return body value" + rs.asString());
-        JsonPath jsonPathEvaluator = rs.jsonPath();
-        JSONObject response_new=new JSONObject(rs.getBody().asString());
-        Assert.assertTrue("Actual Response Code is " + rs.getStatusCode() + " vs Expected Response Code" + responseCode, String.valueOf(rs.getStatusCode()).equals(responseCode));
-        log.info("Able to retrieve response-----" + responseObject);
-        if(responseObject.equalsIgnoreCase("status"))
-        {
-            Actual_message = response_new.getJSONObject("loyaltyCardCreateResponse").getString("loyaltyCardId");
-        }
-        else if(responseObject.equalsIgnoreCase("errorMessage"))
-        {
-            Actual_message=response_new.getJSONObject("error").getString("errorMessage");
-
-        }
-        System.out.println("+++++++++++"+Actual_message);
-        Assert.assertTrue("Actual response is validated",responseMessage.contains(Actual_message.trim()));
-        log.info("Actual response is matching with Expected Response"+responseMessage);
-
-    }
-
-    @And("I make a {string} REST Call with URL {string} and Body from Dictionary Key {string} and random partnerId")
-    public void iMakeARESTCallWithURLAndBodyFromDictionaryKeyAndRandomPartnerId(String callType, String URI, String dictionaryKey) throws URISyntaxException {
-        Response rs = ae.callAPI(callType, Globals.headers, Globals.globalVariables.get(dictionaryKey).toString(), Globals.globalVariables.get("url").toString() + URI);
+    /**
+     * Make arest call with url with random generated partner id and body from dictionary key.
+     *
+     * @param callType      the call type
+     * @param URI           the uri
+     * @param dictionaryKey the dictionary key
+     * @throws URISyntaxException the uri syntax exception
+     */
+    @When("I make a {string} REST Call with URL {string} with random generated partner id and Body from Dictionary Key {string}")
+    public void iMakeARESTCallWithURLWithRandomGeneratedPartnerIdAndBodyFromDictionaryKey(String callType, String URI, String dictionaryKey)
+            throws URISyntaxException {
+        Response rs = callAPI(callType, Globals.headers, Globals.globalVariables.get(dictionaryKey).toString(),
+                              Globals.globalVariables.get("url").toString() + URI);
         Globals.globalVariables.put(dictionaryKey, rs);
     }
+
+    /**
+     * Read the json from file into dictionary key.
+     *
+     * @param jsonFilePath  the json file path
+     * @param dictionaryKey the dictionary key
+     * @throws IOException the io exception
+     */
+    @When("^I read the JSON from file \"([^\"]*)\" into Dictionary Key \"([^\"]*)\"$")
+    public void i_read_the_JSON_from_file_into_Dictionary_Key(String jsonFilePath, String dictionaryKey) throws IOException {
+        String cwd = System.getProperty("user.dir");
+        String jsonPath = cwd + jsonFilePath;
+        Globals.globalVariables.put(dictionaryKey, jsonFileToString(jsonPath));
+    }
+
+    /**
+     * Make arest call with url from dictionary key.
+     *
+     * @param callType      the call type
+     * @param uri           the uri
+     * @param dictionaryKey the dictionary key
+     * @throws URISyntaxException the uri syntax exception
+     */
+    @When("I make a {string} REST Call with URL {string} from Dictionary Key {string}")
+    public void iMakeARESTCallWithURLFromDictionaryKey(String callType, String uri, String dictionaryKey) throws URISyntaxException {
+        Response rs = callAPIs(callType, Globals.headers, "",
+                               Globals.globalVariables.get("url").toString() + uri);
+        Globals.globalVariables.put(dictionaryKey, rs);
+    }
+
+    @Override
+    public String jsonFileToString(String jsonFilePath) throws IOException {
+        return super.jsonFileToString(jsonFilePath);
+    }
+
+    private Response callAPIs(String ApiType, HashMap<String, String> headers, String body, String uri) throws URISyntaxException {
+        Response response = null;
+        String loyaltyCardId = Globals.globalVariables.get("LOYALTY_CARD_ID").toString();
+
+        switch (ApiType.toLowerCase()) {
+        case "get":
+            response = given().relaxedHTTPSValidation().headers(headers).when().get(new URI(uri + "/" + UUID.fromString(loyaltyCardId))).then().extract()
+                    .response();
+            break;
+        case "lookup":
+            response = given().relaxedHTTPSValidation().headers(headers).when().queryParams(getQueryParams()).get(new URI(uri)).then().extract().response();
+            break;
+
+        }
+        return response;
+    }
+
+    private Map<String, String> getQueryParams() {
+        String firstName = Globals.globalVariables.get("FIRST_NAME").toString();
+        String lastName = Globals.globalVariables.get("LAST_NAME").toString();
+        String email = Globals.globalVariables.get("EMAIL").toString();
+        String phone = Globals.globalVariables.get("PHONE").toString();
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("firstName", firstName);
+        queryParams.put("lastName", lastName);
+        queryParams.put("phone", phone);
+        queryParams.put("email", email);
+        return queryParams;
+    }
+
+    private String randomGeneratedPartnerId() {
+        Random r = new Random(System.currentTimeMillis());
+        return String.valueOf(r.nextInt()).replace("-", "");
+    }
+
 }
