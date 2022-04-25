20a75291d07c216b42daa0007252f5596bf8d576
Malav Ashokkumar Parekh <mparekh@HYD-LAP-00298.local>
2022-04-25 12:20:37 +0530

    fixed test runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "features/ebay",
         glue = {"com.napt.eBay.steps"},
-        tags = {"@AddToCart"},
+        tags = "@AddToCart",
         plugin = {"pretty", "html:target/cucumber_target.html", "json:target/cucumber.json", "junit:target/cucumber.xml","rerun:target/failed_scenarios.txt"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
